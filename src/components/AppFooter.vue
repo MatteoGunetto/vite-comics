0fee30e45a5cbdf8a0835602f808fd8547a71801
MatteoGunetto <Matteogunetto@tiscali.it>
2023-05-31 16:27:52 +0200

    footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -5,17 +5,282 @@ export default {
 
 </script>
 <template>
-    <footer>
-        <footer></footer>
+   <footer>
+        <div class="container">
+            <section class="first-section">
+                <nav>
+                    <ul>
+                        <li>
+                            <img src="../assets/img/buy-comics-digital-comics.png" alt="digital_comics">
+                            <a href="#">digital comics</a>
+                        </li>
+                        <li>
+                            <img src="../assets/img/buy-comics-merchandise.png" alt="dc_merchandise">
+                            <a href="#">dc merchandise</a>
+                        </li>
+                        <li>
+                            <img src="../assets/img/buy-comics-subscriptions.png" alt="subscription">
+                            <a href="#">subscription</a>
+                        </li>
+                        <li>
+                            <img src="../assets/img/buy-comics-shop-locator.png" alt="comic_shop_locator">
+                            <a href="#">comic shop locator</a>
+                        </li>
+                        <li>
+                            <img src="../assets/img/buy-dc-power-visa.svg" alt="dc_power_visa">
+                            <a href="#">dc power visa</a>
+                        </li>
+                    </ul>
+                </nav>
+            </section>
+            <section class="second-section">
+                <div>
+                    <ul>
+                        <li>
+                            <h3>dc comics</h3>
+                        </li>
+                        <li>
+                            <a href="#">Characters</a>
+                        </li>
+                        <li>
+                            <a href="#">Comics</a>
+                        </li>
+                        <li>
+                            <a href="#">Movies</a>
+                        </li>
+                        <li>
+                            <a href="#">TV</a>
+                        </li>
+                        <li>
+                            <a href="#">Games</a>
+                        </li>
+                        <li>
+                            <a href="#">Video</a>
+                        </li>
+                        <li>
+                            <a href="#">News</a>
+                        </li>
+                    </ul>
+                    <ul>
+                        <li>
+                            <h3>shop</h3>
+                        </li>
+                        <li>
+                            <a href="#">Shop DC</a>
+                        </li>
+                        <li>
+                            <a href="#">Shop DC Collectibles</a>
+                        </li>
+                    </ul>
+                </div>
+                <div>
+                    <ul>
+                        <li>
+                            <h3>dc</h3>
+                        </li>
+                        <li>
+                            <a href="#">Terms Of Use</a>
+                        </li>
+                        <li>
+                            <a href="#">Privacy policy (New)</a>
+                        </li>
+                        <li>
+                            <a href="#">Ad Choices</a>
+                        </li>
+                        <li>
+                            <a href="#">Advertising</a>
+                        </li>
+                        <li>
+                            <a href="#">Jobs</a>
+                        </li>
+                        <li>
+                            <a href="#">Subscription</a>
+                        </li>
+                        <li>
+                            <a href="#">Talent Workshop</a>
+                        </li>
+                        <li>
+                            <a href="#">CPSC Certificates</a>
+                        </li>
+                        <li>
+                            <a href="#">Ratings</a>
+                        </li>
+                        <li>
+                            <a href="#">Shop Help</a>
+                        </li>
+                        <li>
+                            <a href="#">Contact Us</a>
+                        </li>
+                    </ul>
+                </div>
+                <div>
+                    <ul>
+                        <li>
+                            <h3>sites</h3>
+                        </li>
+                        <li>
+                            <a href="#">DC</a>
+                        </li>
+                        <li>
+                            <a href="#">MAD Magazine</a>
+                        </li>
+                        <li>
+                            <a href="#">DC Kids</a>
+                        </li>
+                        <li>
+                            <a href="#">DC Universe</a>
+                        </li>
+                        <li>
+                            <a href="#">DC Power Visa</a>
+                        </li>
+                        
+                    </ul>
+                </div>
+                <img src="../assets/img/dc-logo-bg.png" alt="">
+            </section>
+            <section class="third-section">
+                <div>
+                    <div>
+                        <button>
+                            sign-up now!
+                        </button>
+                    </div>
+                    <div>
+                        <a href="#">follow us</a>
+                        <img src="../assets/img/footer-facebook.png" alt="facebook">
+                        <img src="../assets/img/footer-twitter.png" alt="twitter">
+                        <img src="../assets/img/footer-youtube.png" alt="youtube">
+                        <img src="../assets/img/footer-pinterest.png" alt="pinterest">
+                        <img src="../assets/img/footer-periscope.png" alt="periscope">
+                    </div>
+                </div>
+            </section>
+        </div>
     </footer>
 </template>
 
 <style lang="scss" scoped>
-
+footer{
+    height: 850px;
+    background-image: url('../assets/img/footer-bg.jpg');
+    background-repeat: no-repeat;
+    background-size: cover;
+    
+    div.container{
+        height: 100%;
+        section.first-section{
+            height: 170px;
+            background-color: #0282f9;
+            display: flex;
+            text-align: center;
+            justify-content: center;
+            z-index: 1;
+            position: relative;
+            nav{
+                height: 100%;
+                max-width: 1000px;
+                margin: 0 auto;
+                ul{
+                    display: flex;
+                    text-align: center;
+                    justify-content: space-around;
+                    height: 100%;
+                    li{
+                        list-style: none;
+                        height: 100%;
+                        display: flex;
+                        align-items: center;
+                        justify-content: space-between;
+                        padding: 0 20px;
+                        a{
+                            text-decoration: none;
+                            color: white;
+                            text-transform: uppercase;
+                            font-size: 16px;
+                            padding-left: 10px;
+                        }
+                        img{
+                            height: 40%;
+                        }
+                    }
+                }
+            }
+        }
+        section.second-section{
+            height: 530px;
+            max-width: 1000px;
+            margin: 0 auto;
+            display: flex;
+            padding: 40px 0;
+            overflow: hidden;
+                div{
+                    margin-right: 25px;
+                    ul{
+                        li{
+                            color: white;
+                            list-style: none;
+                            h3{
+                                text-transform: uppercase;
+                                margin-bottom: 8px;
+                            }
+                            a{
+                                color: inherit;
+                                margin-bottom: 5px;
+                                text-decoration: none;
+                                font-size: 15px;
+                            }
+                        }
+                    }
+                    ul:first-of-type{
+                        margin-bottom: 15px;
+                    }
+                }
+                div:last-of-type{
+                    flex-grow: 1;
+                }
+                img{
+                    height: 650px;
+                    align-self: center;
+                }
+        }
+        section.third-section{
+            height: 150px;
+            background-color: #303030;
+            div{
+                max-width: 1000px;
+                height: 100%;
+                margin: 0 auto;
+                display: flex;
+                justify-content: space-between;
+                align-items: center;
+                div:first-child{
+                    flex-grow: 1;
+                    button{
+                        color: white;
+                        text-transform: uppercase;
+                        border: 2px solid #0282f9;
+                        padding: 10px 15px;
+                        background-color: transparent;
+                        cursor: pointer;
+                    }
+                }
+                div:last-child{
+                    a{
+                        text-decoration: none;
+                        color: #0282f9;
+                        text-transform: uppercase;
+                        margin-right: 5px;
+                    }
+                    img{
+                        height: 25px;
+                        margin: 0 5px;
+                    }
+                }
+            }
+        }
+    }
+}
 </style>
-
-
-
 
 
 
